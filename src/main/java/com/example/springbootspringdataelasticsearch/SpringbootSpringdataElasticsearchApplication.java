5d146dd2e3da5de7ad67fb48da2e69cb36035263
Mfarzana <mfarzana1133@gmail.com>
2020-06-25 16:08:35 +0900

    Initial Commit

---
+++ src/main/java/com/example/springbootspringdataelasticsearch/SpringbootSpringdataElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootspringdataelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringdataElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringdataElasticsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootspringdataelasticsearch/SpringbootSpringdataElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootspringdataelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringdataElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
